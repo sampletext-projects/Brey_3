187564ccf819040e110f1d43ea58e67fcebcada3
sampletext32 <sampletext32@bk.ru>
2020-04-21 20:19:07 +0300

    Add project

---
+++ Brey_3/main.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	cout << "Программу сделала Белевцева Дарья Юрьевна студент группы УМЛ-112\n";
+	cout << "Программа для расчёта полинома\n";
+
+	for (int i = -5; i <= 11; i++)
+	{
+		double x = i / 10.;
+		double p = 0;
+		double m = 1;
+		for (int k = 0; k < 100; k++)
+		{
+			p += m;
+			m *= x;
+		}
+		cout << "P(" << x << ") = " << p << endl;
+	}
+
+	system("pause");
+
+	return 0;
+}
